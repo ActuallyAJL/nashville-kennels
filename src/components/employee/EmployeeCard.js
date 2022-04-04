67e66c0fc1f227596e8c67d2cb2f9e08f6a0f0be
ActuallyAJL <zero3260@hotmail.com>
2022-04-04 15:39:11 -0500

    continued through lesson

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employee.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h3 className="employee__name">Ebenezer Whitehead</h3>
+        <div className="employee__role">Manager</div>
+    </section>
+)
---
+++ src/components/location/LocationCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">Old Hickory</h3>
+        <div className="location__address">999 Old Hickory Blvd</div>
+    </section>
+)
