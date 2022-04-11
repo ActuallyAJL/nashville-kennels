56533279e7747390490610641791719f5126e788
ActuallyAJL <zero3260@hotmail.com>
2022-04-11 11:33:31 -0500

    added authentication requirements for site users

--- kennels/src/components/ApplicationViews.js
+++ kennels/src/components/ApplicationViews.js
@@ -1,30 +1,105 @@
-import React from "react"
-import { Route, Routes} from "react-router-dom"
-import { Home } from "./Home"
-import { AnimalList } from './animal/AnimalList.js'
-import { AnimalDetail } from "./animal/animalDetail.js"
-import { AnimalForm } from './animal/AnimalForm.js'
-import { EmployeeList } from './employee/EmployeeList.js'
-import { LocationList } from './location/LocationList.js'
-import { CustomerList } from './customer/CustomerList.js'
-import { MadLib } from './madlib/MadLib.js'
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Home } from "./Home";
+import { AnimalList } from "./animal/AnimalList.js";
+import { AnimalDetail } from "./animal/animalDetail.js";
+import { AnimalForm } from "./animal/AnimalForm.js";
+import { EmployeeList } from "./employee/EmployeeList.js";
+import { LocationList } from "./location/LocationList.js";
+import { CustomerList } from "./customer/CustomerList.js";
+import { MadLib } from "./madlib/MadLib.js";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 
-export const ApplicationViews = () => {
-    return (
-        <>
-            <Routes>
-                {/* Render the location list when http://localhost:3000/ */}
-                <Route exact path="/" element={<Home />} />
+export const ApplicationViews = ({ isAuthenticated, setIsAuthenticated }) => {
+  const PrivateRoute = ({ children }) => {
+    return isAuthenticated ? children : <Navigate to="/login" />;
+  };
 
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route exact path="/animals" element={<AnimalList />} />
-                <Route path="/animals/details/:animalId" element={<AnimalDetail />} />
-                <Route path="/animals/create" element={<AnimalForm />} />
-                <Route path="/locations" element={<LocationList />} />
-                <Route path="/customers" element={<CustomerList />} />
-                <Route path="/employees" element={<EmployeeList />} />
-                <Route path="/madlib" element={<MadLib />} />
-            </Routes>
-        </>
-    )
-}
+  const setAuthUser = (user) => {
+    sessionStorage.setItem("kennel_customer", JSON.stringify(user));
+    setIsAuthenticated(sessionStorage.getItem("kennel_customer") !== null);
+  };
+
+  return (
+    <>
+      <Routes>
+        {/* Render the location list when http://localhost:3000/ */}
+        <Route
+          exact
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Render the animal list when http://localhost:3000/animals */}
+        <Route
+          exact
+          path="/animals"
+          element={
+            <PrivateRoute>
+              <AnimalList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/animals/details/:animalId"
+          element={
+            <PrivateRoute>
+              <AnimalDetail />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/animals/create"
+          element={
+            <PrivateRoute>
+              <AnimalForm />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/locations"
+          element={
+            <PrivateRoute>
+              <LocationList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/customers"
+          element={
+            <PrivateRoute>
+              <CustomerList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/employees"
+          element={
+            <PrivateRoute>
+              <EmployeeList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/login"
+          element={<Login setAuthUser={setAuthUser} />}
+        />
+        <Route exact path="/register" element={<Register />} />
+        <Route
+          path="/madlib"
+          element={
+            <PrivateRoute>
+              <MadLib />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
